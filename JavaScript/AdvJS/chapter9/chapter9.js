77b597d9c857e89880eec066ba94228d82c421ab
Dorette <dorette.schutte86@gmail.com>
2023-07-14 11:22:11 +0200

    chapter9

---
+++ JavaScript/AdvJS/chapter9/chapter9.js
@@ -0,0 +1,13 @@
+//browser object model holds all the methods and properties for JavaScript (BOM)
+//dom contains the html elements on the webpage
+
+//queryselector()
+//find h1 in html and everything thats in it
+
+// console.dir(document.querySelector("h1"));
+
+//queryselectorAll()
+//selects all and return as a list of all the "output" class
+
+// console.dir(document.querySelectorAll(".output"));
+
